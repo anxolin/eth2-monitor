de593df8fa0743aa2689ce825643480b262f0b52
Anxo Rodriguez <anxolin@gmail.com>
2022-06-11 14:04:40 +0100

    Add corutines and post messages to telegram

--- validator.py
+++ validator.py
@@ -1,10 +1,12 @@
-import requests
 import telegram
+import logging
+import yaml
+import asyncio
+import signal
+import requests
 import time
 import json
-import logging
-import atexit
-import yaml
+import os
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 log = logging.getLogger(__name__)
@@ -15,36 +17,70 @@ with open('config.yaml', 'r') as f:
 validator_url = config['validator_url']
 validator_active = {}
 
-@atexit.register
-def say_goodbye():
-    bot = telegram.Bot(token=config['TELEGRAM']['ACCESS_TOKEN'])
-    bot.sendMessage(chat_id=config['CHAT']['ID'], text=f'Validator monitor leave, bye~')
+loop = asyncio.get_event_loop()
+bot = telegram.Bot(
+    token=config['TELEGRAM']['ACCESS_TOKEN']
+)
+chat_id = chat_id=config['CHAT']['ID']
 
-if __name__ == '__main__':
-    # print(config['TELEGRAM']['ACCESS_TOKEN'])
-    bot = telegram.Bot(token=config['TELEGRAM']['ACCESS_TOKEN'])
-    bot.sendMessage(chat_id=config['CHAT']['ID'], text=f"`Validator` monitor *start*\n```{json.dumps(validator_url, indent=2)}```", parse_mode='MarkdownV2')
+async def main():
+    async with bot:
+        user = await bot.get_me()
+        log.info('[%s] Telegram bot "%s" up', user.username, user.first_name)
+        await bot.send_message(
+            chat_id=chat_id,
+            text=f"☀️ Validator Monitor *RESTARTED*",
+            parse_mode='MarkdownV2'
+        )
+
+
     while True:
-        for url in validator_url:
-            res = requests.get(url)
-            try:
-                res_json = res.json()
-            except json.decoder.JSONDecodeError:
-                log.exception(res.text)
-                continue
-            try:
-                for data in res_json['data']:
-                    log.info('%s, %s',data[1], data[3])
-                    index = data[1] # == k
-                    state = data[3]
-                    if (state == 'active_online') == validator_active.get(index, True):
-                        continue
-                    message = f'<b>{index}</b> change to {state}'
-                    message = message.replace('active_online', 'active_online👍')
-                    message = message.replace('active_offline', 'active_offline🗿')
-                    bot.send_message(chat_id=config['CHAT']['ID'], text=message, parse_mode='HTML')
-                    validator_active[index] = not validator_active.get(index, True)
-            except IndexError:
-                log.exception(res.text)
-                continue
-        time.sleep(5)
+        # for url in validator_url:
+        #     res = requests.get(url)
+        #     try:
+        #         res_json = res.json()
+        #     except json.decoder.JSONDecodeError:
+        #         log.exception(res.text)
+        #         continue
+        #     try:
+        #         for data in res_json['data']:
+        #             log.info('%s, %s',data[1], data[3])
+        #             index = data[1] # == k
+        #             state = data[3]
+        #             if (state == 'active_online') == validator_active.get(index, True):
+        #                 continue
+        #             message = f'<b>{index}</b> change to {state}'
+        #             message = message.replace('active_online', 'active_online👍')
+        #             message = message.replace('active_offline', 'active_offline🗿')
+        #             bot.send_message(chat_id=config['CHAT']['ID'], text=message, parse_mode='HTML')
+        #             validator_active[index] = not validator_active.get(index, True)
+        #     except IndexError:
+        #         log.exception(res.text)
+        #         continue
+        # time.sleep(5)
+        await asyncio.sleep(5)
+
+async def say_goodbye():
+    log.info("Have a good day Ser!")
+    async with bot:
+        await bot.send_message(
+            chat_id=chat_id,
+            text=f"💤 Validator Monitor *SHUTDOWN*\. Have a nice day Ser\!",
+            parse_mode='MarkdownV2'
+        )
+
+def stop():
+    log.info("Shutting down")
+    loop.run_until_complete(say_goodbye())
+    log.info("Shutdown complete ...")
+
+signal.signal(signal.SIGTERM, stop)
+
+if __name__ == '__main__':        
+    try:
+        loop.run_until_complete(main())
+    except KeyboardInterrupt:  # pragma: no branch
+        pass
+    finally:
+        stop()
+        loop.close()
